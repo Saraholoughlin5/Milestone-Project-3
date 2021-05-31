84beb445c54f5c69fd793b31a7e12b496bd6b513
Saraholoughlin5 <saraholoughlin5@gmail.com>
2021-05-31 13:36:29 +0000

    Add join.html and form

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,6 +23,11 @@ mongo = PyMongo(app)
 def get_recipe():
     recipe = mongo.db.recipe.find()
     return render_template("recipe.html", recipe=recipe)
+
+
+@app.route("/join", methods=["GET", "POST"])
+def join():
+    return render_template("join.html")
 
 
 if __name__ == "__main__":
