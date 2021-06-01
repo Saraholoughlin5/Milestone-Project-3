929e798df42d8704225a3f4ec93782ee072808e2
Saraholoughlin5 <saraholoughlin5@gmail.com>
2021-06-01 21:13:40 +0000

    Update login flash message and comments

--- app.py
+++ app.py
@@ -49,27 +49,26 @@ def join():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        # check if username exists in db
+        # check that username exists in database
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
         if existing_user:
-            # ensure hashed password matches user input
             if check_password_hash(
+                # check that hashed password matches users input
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(
+                    flash("Welcome, {}!".format(
                         request.form.get("username")))
-                    return redirect(url_for(
-                        "profile", username=session["user"]))
+                    return redirect(url_for("login"))
 
             else:
-                # invalid password match
+                # invalid user password
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
 
         else:
-            # username doesn't exist
+            # invalid username or username does not exist
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
 
