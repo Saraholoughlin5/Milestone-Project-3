4edc369ad4da1809858bc1940f763b80857dd426
Saraholoughlin5 <saraholoughlin5@gmail.com>
2021-06-01 20:24:49 +0000

    Set up login page and log in/sign up links

--- app.py
+++ app.py
@@ -46,6 +46,11 @@ def join():
     return render_template("join.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
