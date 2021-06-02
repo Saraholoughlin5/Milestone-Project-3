4019e686ba59dd18bdfbaa8ddc8c50fa76f63aed
Saraholoughlin5 <saraholoughlin5@gmail.com>
2021-06-02 17:50:46 +0000

    Add if/else statement to navbar menus

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.fixed-action-btn').floatingActionButton({direction: "left", hoverEnabled: false});
+    $('.modal').modal();
   });
  
