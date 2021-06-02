e2c76c07786d5ec8332dee49cfb39d3a67d41674
Saraholoughlin5 <saraholoughlin5@gmail.com>
2021-06-02 18:42:58 +0000

    Add and set-up collapsible for recipes

--- static/js/script.js
+++ static/js/script.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.fixed-action-btn').floatingActionButton({direction: "left", hoverEnabled: false});
     $('.modal').modal();
+    $('.collapsible').collapsible();
   });
  
