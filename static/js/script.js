393c82af5881f1b3bd10fb2fafb9bf4efacbe548
Saraholoughlin5 <saraholoughlin5@gmail.com>
2021-06-03 15:45:15 +0000

    Format layout of add_recipe form and recipe collapsible

--- static/js/script.js
+++ static/js/script.js
@@ -3,5 +3,6 @@ $(document).ready(function(){
     $('.fixed-action-btn').floatingActionButton({direction: "left", hoverEnabled: false});
     $('.modal').modal();
     $('.collapsible').collapsible();
+    $('input#input_text, textarea#textarea2').characterCounter();
   });
  
