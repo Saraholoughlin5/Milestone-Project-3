fd382344f1dd33491c920410934c1c24298d8614
Saraholoughlin5 <saraholoughlin5@gmail.com>
2021-06-07 12:34:11 +0000

    Add 'Edit' and 'Delete' buttons to collapsible

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,24 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
-    $('.fixed-action-btn').floatingActionButton({direction: "left", hoverEnabled: false});
+/* Jquery initialization - Materialize */
+
+$(document).ready(function () {
+    $('.sidenav').sidenav({
+        edge: "right"
+    });
+    $('.fixed-action-btn').floatingActionButton({
+        direction: "left",
+        hoverEnabled: false
+    });
     $('.modal').modal();
     $('.collapsible').collapsible();
     $('input#input_text, textarea#textarea2').characterCounter();
     $('select').formSelect();
     $('.tooltipped').tooltip();
     $('.datepicker').datepicker({
-        format: "dd mmm yyyy", 
+        format: "dd mmm yyyy",
         yearRange: 2,
         showClearBtn: true,
         i18n: {
             done: "Select"
         },
     });
-  });
+});
